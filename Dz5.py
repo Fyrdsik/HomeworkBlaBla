eff960d0c5fb02ba95ba016f96e946ad97e29b44
Fyrdsik <timuraspect@gmail.com>
2025-12-18 15:24:44 +0200

    Initial commit

---
+++ Dz5.py
@@ -0,0 +1,13 @@
+text = input("Введите текст: ")
+
+reversed_text = text[::-1]
+
+result = "\n".join(reversed_text)
+print(result)
+
+
+
+#2 Задание
+
+#for number in range(1, 11):
+#    print(f"{number}\n")
